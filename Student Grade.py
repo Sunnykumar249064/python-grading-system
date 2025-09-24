e6d1429702e08ed573078639cb0114815f70e98a
Sunnykumar249064 <sunnyk12345678k@gmail.com>
2025-09-24 23:42:57 +0530

    Add files via upload

---
+++ Student Grade.py
@@ -0,0 +1,11 @@
+marks = int (input("Enter your marks: "))
+if marks >= 85:
+    print("A+")
+elif marks >= 70:
+    print("B+")
+elif marks >= 50:
+    print("C")
+elif marks >= 33:
+    print("D")
+else:
+    print("Fail!")
